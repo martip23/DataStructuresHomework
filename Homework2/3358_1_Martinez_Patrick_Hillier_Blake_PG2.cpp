a477e33e696ec19ef7358b70e74f65f3ec57b61f
canadianGinge <blakehillier@mac.com>
2018-02-11 22:46:55 -0600

    Deleted extra copy of program

--- Homework2/3358_1_Martinez_Patrick_Hillier_Blake_PG2.cpp
+++
@@ -1,9 +0,0 @@
-// Authors: Patrick Martinez
-//          Blake Hillier Serial: 20 
-// Due Date : Monday, February 19, 2018
-// Programming Assignment Number 2
-// Spring 2018 - CS 3358 - 1 
-//
-// Instructor: Husain Gholoom. 
-//
-// <Brief description of the purpose of the program>
