b4fb39a14f1fcafc44b541f227190f8e55930773
Blake Hillier <blakehillier@mac.com>
2018-02-11 22:30:56 -0600

    Created folder and file for program 2

---
+++ Homework2/3358_1_Martinez_Patrick_Hillier_Blake_PG2.cpp
@@ -0,0 +1,9 @@
+// Authors: Patrick Martinez
+//          Blake Hillier Serial: 20 
+// Due Date : Monday, February 19, 2018
+// Programming Assignment Number 2
+// Spring 2018 - CS 3358 - 1 
+//
+// Instructor: Husain Gholoom. 
+//
+// <Brief description of the purpose of the program>
