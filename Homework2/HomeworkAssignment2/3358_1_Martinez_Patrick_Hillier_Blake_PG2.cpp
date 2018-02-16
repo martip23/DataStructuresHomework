ec1483dcef583cf29625938f009691e22a000d29
martip23 <patrickmartinez77@gmail.com>
2018-02-16 13:55:53 -0600

    Added comments

--- Homework2/HomeworkAssignment2/3358_1_Martinez_Patrick_Hillier_Blake_PG2.cpp
+++ Homework2/HomeworkAssignment2/3358_1_Martinez_Patrick_Hillier_Blake_PG2.cpp
@@ -1,34 +1,59 @@
-// Authors: Patrick Martinez
-//          Blake Hillier Serial: 20 
+// Authors: Patrick Martinez Serial: 24
+//          Blake Hillier Serial: 20
 // Due Date : Monday, February 19, 2018
 // Programming Assignment Number 2
-// Spring 2018 - CS 3358 - 1 
+// Spring 2018 - CS 3358 - 1
 //
-// Instructor: Husain Gholoom. 
+// Instructor: Husain Gholoom.
 //
-// <Brief description of the purpose of the program>
+// This program's purpose is to assist students in estimating their current
+// course grade in CS 3358. It allows the user to create and populate a vector
+// array with any amount of assignment grades and two test grades. It also
+// allows the user to display either assignment grades, test grades or overall
+// grades in the course. It also has an option to delete grades.
 
 // Libraries
-#include <iostream>
-#include <vector>
+#include <iostream> // Provides cout, cin and other io stream operations
+#include <vector>   // Provides vector type to hold grades
+
 using namespace std;
 
 // Classes
+/**********************************************
+myGrades class
+    This class is meant to hold program and test grades to perform calculations.
+    The public methods will allow manipulation of data and presentation of data.
+
+-vector<int> programGrades  - Holds program grades
+-vector<int> testGrades     - Holds test grades
+***********************************************/
 class myGrades {
-    vector<int> programGrades;
-    vector<int> testGrades;
- 
+
+    private:
+    vector<int> programGrades;  // Vector to hold program grades
+    vector<int> testGrades;     // Vector to hold test grades
+
     public:
-       void setProgramGrade();
-       void setTestGrade();
-       void const showProgramGrades();
-       void const showTestGrades();
-       void const showOverallGrade();
-       void deleteProgramGrade();
-       void deleteTestGrade(); 
+
+        // Displays prompt and adds a program grade
+        void setProgramGrade();
+        // Displays prompt and adds a test grade
+        void setTestGrade();
+        // Shows programming assignment grades and average of these grades
+        void const showProgramGrades();
+        // Shows test grades and average of these grades
+        void const showTestGrades();
+        // Shows all grades and overall total
+        void const showOverallGrade();
+        // Displays prompt to remove a program grade equal to input=
+        void deleteProgramGrade();
+        // Displays prompt to remove a test grade equal to input
+        void deleteTestGrade();
 };
 
 int main() {
+    cout << "Welcome to Patrick and Blake's grades program.\n" << endl;
+
     // Creates new myGrades object
     myGrades Grades;
     // If true, tells loop to run again
@@ -52,8 +77,8 @@ int main() {
 
         try {
             // Checks if userChoice contains an integer 1-7 or 9
-            if (false) { 
-                cout << "ERROR: Invalid Choice\n Input needs to be an integer"; 
+            if (false) {
+                cout << "ERROR: Invalid Choice\n Input needs to be an integer";
                 cout << endl;
             } else
                 cout << "ERROR: Invalid Choice" << endl;
@@ -90,30 +115,67 @@ int main() {
 }
 
 // Functions
+/*******************************************************************
+ setProgramGrade
+ Input: none
+ Returns: none
+ Displays a prompt for an int from 1-10 and pushes it to the assignment grades
+ array.
+ *******************************************************************/
 void myGrades::setProgramGrade() {
 
 }
-
+/*******************************************************************
+ setTestGrade
+ Input: none
+ Returns: none
+ Displays a prompt for an int from 1-25 and passes it to the test grades array
+ *******************************************************************/
 void myGrades::setTestGrade() {
 
 }
-
+/*******************************************************************
+ showProgramGrades
+ Input: none
+ Returns: none
+ Shows program grades and program grades average out of 10.
+ *******************************************************************/
 void const myGrades::showProgramGrades() {
 
 }
-
+/*******************************************************************
+ showTestGrades
+ Input: none
+ Returns: none
+ Shows test grades and test grades average out of 20.
+ *******************************************************************/
 void const myGrades::showTestGrades() {
 
 }
-
+/*******************************************************************
+ showOverallGrade
+ Input: none
+ Returns: none
+ Shows all grades, average for each, and an overall total.
+ *******************************************************************/
 void const myGrades::showOverallGrade() {
 
 }
-
+/*******************************************************************
+ deleteProgramGrade
+ Input: none
+ Returns: none
+ Prompts for a grade to delete. Deletes grade equal to input.
+ *******************************************************************/
 void myGrades::deleteProgramGrade() {
 
 }
-
+/*******************************************************************
+ deleteTestGrade
+ Input: none
+ Returns: none
+ Prompts for a grade to delete. Deletes grade equal to input.
+ *******************************************************************/
 void myGrades::deleteTestGrade() {
 
 }
