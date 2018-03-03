e4ff3b15966e25b33a0d9a620a26f31d7146e870
martip23 <patrickmartinez77@gmail.com>
2018-03-03 17:44:14 -0600

    Added implementation comments, changed sort to sortList to prevent cdstlib conflict

--- Homework3/HomeworkAssignment3/Martinez_Patrick_Hillier_Blake_3358_1_PG3_LL.cpp
+++ Homework3/HomeworkAssignment3/Martinez_Patrick_Hillier_Blake_3358_1_PG3_LL.cpp
@@ -1,22 +1,33 @@
 /*******************************************
  Roster Numbers: 20 24
- 
+
  Authors: Blake Hillier and Patrick Martinez
 
  Programming Assignment Number 3
 
- Spring 2018 - CS 3358 - 251?
+ Spring 2018 - CS 3358 - 251
 
  Due Date: March 5, 2018
 
  Instructor: Husain Gholoom
 
- Description here
+ This program is a demonstration of a linked list. It will
+ a) Build 2 linked lists of 15 random integers from 1-19
+ b) Create a new list that contains all elements of list 1 and list 2 then
+    display
+ c) It will display the number of occurrences of the first element in list 3
+ d) Create a new list by removing all duplicates in list 3 and display this list
+ e) Display number of elements in list 4
+ f) Display list 4 backwards
+ g) Move the last element of list4 and place it after the 3rd element in the
+    same list, then display
+ h) Sort list 4 in ascending order, and display.
+
  *******************************************/
- 
-#include <cstdlib> // Provides srand and rand
-#include <ctime> // Provides time for random seed
-#include <iomanip> // Provides setw and precision 
+
+#include <cstdlib>  // Provides srand and rand
+#include <ctime>    // Provides time for random seed
+#include <iomanip>  // Provides setw and precision
 #include <iostream> // Provides cin, cout and endl
 
 using namespace std;
@@ -54,9 +65,9 @@ class NumberList {
         // Move the last element behind the element at destinationIndex
         void moveElement(int destinationIndex);
         // Sorts the list in increasing number
-        void sort();
-}; 
-        
+        void sortList();
+};
+
 int main() {
     bool run = true;
     char userChoice;
@@ -70,15 +81,17 @@ int main() {
         // Displays lists on screen
         list_1.display("list_1");
         list_2.display("list_2");
-  
-        // Creates list from numbers in list_1 and then list_2 and displays it on screen
+
+        // Creates list from numbers in list_1 and
+        //  then list_2 and displays it on screen
         NumberList list_3(list_1, list_2);
         list_3.display("list_3");
 
         // Displays number of times 1st number appears in list
         list_3.numOccurance(1);
 
-        // Creates list based off of list_3 with no duplicate numbers and displays it on screen
+        // Creates list based off of list_3 with no duplicate
+        // numbers and displays it on screen
         NumberList list_4(list_3);
         list_4.display("list_4");
 
@@ -87,12 +100,13 @@ int main() {
 
         // Displays list backwards on screen
         list_4.displayBackwards();
- 
-        // Moves the last element and places it after the 3rd one and displays it on screen
+
+        // Moves the last element and places it after the 3rd
+        // one and displays it on screen
         list_4.moveElement(3);
         list_4.display("list_4 after moving last element to the 4th position");
 
-        // Sorts list in increasing order and displays it on screen 
+        // Sorts list in increasing order and displays it on screen
         list_4.sort();
         list_4.display("list_4 sorted");
 
@@ -102,10 +116,11 @@ int main() {
         list_3.clearList();
         list_4.clearList();
 
-        cout << "Would you like to run this again? (Y/y = yes or N/n = no)" << endl;
+        cout << "Would you like to run this again? (Y/y = yes or N/n = no)"
+            << endl;
         cin >> userChoice;
 
-        // Checks user's input to see wether to run program again or not
+        // Checks user's input to see whether to run program again or not
         if (userChoice == 'y' || userChoice == 'Y')
             run = true;
         else if (userChoice == 'n' || userChoice == 'N')
@@ -116,26 +131,29 @@ int main() {
         }
 
     } while (run);
- 
-    cout << "This LL Program is Implimented by:\nBlake Hillier and Patrick";
+
+    cout << "This LL Program is Implemented by:\nBlake Hillier and Patrick";
     cout << "Martinez - March 5th, 2018" << endl;
 }
 
 // Functions
 
-// Creates list with 15 #'s between 1-19
+/*******************************************************************
+ NumberList default constructor
+ Creates a list with 15 numbers between 1-19
+ *******************************************************************/
 NumberList::NumberList() {
     const int INIT_LENGTH = 15;
 
     // Creates first node
     ListNode * newNode = new ListNode;
-    newNode -> prv = NULL; 
+    newNode -> prv = NULL;
     newNode -> num = rand()%19 + 1;
 
     // Points head to the first node
     head = newNode;
     // Creates variable prvNode and points it at the first node stored in head
-    ListNode * prvNode = new ListNode; 
+    ListNode * prvNode = new ListNode;
     prvNode = head;
 
     /************************************************************
@@ -145,30 +163,45 @@ NumberList::NumberList() {
                          creates a number for the new node
                          points the new node also to null
                          assigns the new node to prvNode
-     ************************************************************/ 
+     ************************************************************/
     for (int i = 1; i < INIT_LENGTH; i++) {
         ListNode * newNode = new ListNode;
         prvNode -> nxt = newNode;
         newNode -> prv = prvNode;
         newNode -> num = rand()%19 + 1;
-        newNode -> nxt = NULL; 
-        prvNode = newNode; 
+        newNode -> nxt = NULL;
+        prvNode = newNode;
     }
     // Points the last node to the tail
     tail = prvNode;
 }
 
-// Creates lists based on list1 and then list2
+/*******************************************************************
+ NumberList 2 parameter constructor
+ Input: two Number List objects
+ Output: None
+ Creates new list based on list1 and list2
+ *******************************************************************/
 NumberList::NumberList(NumberList const &list1, NumberList const &list2) {
     head = NULL;
 }
 
-// Creates list based on list1 with no duplicate numbers
+/*******************************************************************
+ Numberlist Single Paramter constructor
+ Input: 1 numberList
+ Output: None
+ Creates list based on list1 with no duplicate values.
+ *******************************************************************/
 NumberList::NumberList(NumberList const &list1) {
     head = NULL;
 }
 
-// Deletes all structures in list and points head/tail to null
+/*******************************************************************
+ clearList
+ Input: None
+ Output: None
+ Clears the list.
+ *******************************************************************/
 void NumberList::clearList() {
     if (!head) { // TODO: Delete if statement once all constructors have been implemented
         ListNode * rptr = head;
@@ -186,7 +219,12 @@ void NumberList::clearList() {
     }
 }
 
-// Displays items in list
+/*******************************************************************
+ Display
+ Input: none
+ Returns: none
+ Displays all items in list.
+ *******************************************************************/
 void NumberList::display(string listName) {
     if (!head) { // TODO: Delete if statement once all constructors have been implemented
     ListNode * newNode = new ListNode;
@@ -200,7 +238,8 @@ void NumberList::display(string listName) {
     ListNode * tptr = head;
     cout << listName << ": " << tptr -> num;
 
-    // If tptr points to another node, assing tptr to that node and print its number
+    // If tptr points to another node,
+    // assing tptr to that node and print its number
     while (tptr -> nxt) {
         tptr = tptr -> nxt;
         cout << " " << tptr -> num;
@@ -209,22 +248,47 @@ void NumberList::display(string listName) {
     cout << endl;
 }
 
-// Finds number of Occurances for the number at position index
-void NumberList::numOccurance(int index) {
+/*******************************************************************
+ numOccurrence
+ Input: index
+ Returns: none
+ Displays the number of occurrences of for the item at index position.
+ *******************************************************************/
+void NumberList::numOccurence(int index) {
 }
 
-// Finds length of list
+/*******************************************************************
+ findLength
+ Input: none
+ Returns: none
+ Displays the length of the list.
+ *******************************************************************/
 void NumberList::findLength() {
 }
 
-// Displays the list backwards
+/*******************************************************************
+ displayBackwards
+ Input: none
+ Returns: none
+ Displays the list backwards.
+ *******************************************************************/
 void NumberList::displayBackwards() {
 }
 
-// Move the last element behind the element at destinationIndex
+/*******************************************************************
+ moveElement
+ Input: destinationIndex
+ Returns: none
+ Moves the last element behind the element at destinationIndex
+ *******************************************************************/
 void NumberList::moveElement(int destinationIndex) {
 }
 
-// Sorts the list in increasing number
-void NumberList::sort() {
+/*******************************************************************
+ sort
+ Input: none
+ Returns: none
+ Sorts the list in ascending order
+ *******************************************************************/
+void NumberList::sortList() {
 }
