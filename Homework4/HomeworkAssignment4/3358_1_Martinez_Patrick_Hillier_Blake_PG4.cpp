149776a4fc7431e57c9ce6aff35f0cd36e2aceb6
martip23 <patrickmartinez77@gmail.com>
2018-03-27 16:27:13 -0500

    Merging files

---
+++ Homework4/HomeworkAssignment4/3358_1_Martinez_Patrick_Hillier_Blake_PG4.cpp
@@ -0,0 +1,27 @@
+//      Roster Numbers: 24 20
+//      Authors: Patrick Martinez, Blake Hillier
+//      Due Date: 3/28/2018
+//      Programming Assignment Number: 4
+//
+//      Spring 2018 - CS 3358 - 251
+//
+//      Instructor: Husain Gholoom.
+//
+//      For this assignment we will implement a stack and queue ADT and
+//      use these ADTs to compare strings. We will use a stack to determine
+//      whether two strings are the same and a queue to determine whether two
+//      strings are the reverse of each other. The program will have a terminal
+//      IO for input and output.
+
+class Stack {
+
+};
+
+class Queue {
+
+};
+
+int main () {
+
+return 0;
+}
