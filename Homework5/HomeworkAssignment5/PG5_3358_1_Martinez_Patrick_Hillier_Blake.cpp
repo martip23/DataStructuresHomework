283a4c8fd83ef810e01156a0e795c2767110834c
martip23 <patrickmartinez77@gmail.com>
2018-04-04 10:00:23 -0500

    Added most cout statements

--- Homework5/HomeworkAssignment5/PG5_3358_1_Martinez_Patrick_Hillier_Blake.cpp
+++ Homework5/HomeworkAssignment5/PG5_3358_1_Martinez_Patrick_Hillier_Blake.cpp
@@ -10,4 +10,35 @@
  Instructor: Husain Gholoom
 
  //TODO: Description
- *******************************************************************************/ 
+ ******************************************************************************/
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main() {
+
+     bool again = true;
+     char choice;
+
+     cout << "Thinking Recursively\n"
+          << "\n"
+          << "The function of this program is to\n"
+          << "use recursion in order to perform the following operations   :-\n"
+          << "\n"
+          << "    1. Display squares of integers in ascending order from 1 to\n"
+          << "         the last element in the array\n"
+          << "    2. Raise the first number to a power of 2\n"
+          << "    3. Find the array's max value.\n"
+          << "    4. Sort the array in descending order\n"
+          << "    5. Calculate the sum of digits\n"
+          << "    6. Determine if a number is prime (Processes all array\n"
+          << "         values)\n";
+
+    while(again) {
+        again = false;
+    }
+
+    cout << "\n\nPatrick Martinez & Blake Hillier - Tweak Programming Developer"
+         << "\nApril 2018\n\n";
+ }
