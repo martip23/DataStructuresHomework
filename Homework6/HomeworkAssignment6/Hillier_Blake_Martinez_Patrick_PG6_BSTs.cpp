052e21bc43dff3081e52603a12eea88866b27ecc
martip23 <patrickmartinez77@gmail.com>
2018-04-17 10:03:36 -0500

    Added initial HW files and comments

---
+++ Homework6/HomeworkAssignment6/Hillier_Blake_Martinez_Patrick_PG6_BSTs.cpp
@@ -0,0 +1,20 @@
+//  Roster Numbers: 20 24
+//
+//  Authors: Patrick Martinez & Blake Hillier
+//  Due Date: 4/25/2018
+//  Programming Assignment Number 6
+//
+//  Spring 2018 - CS 3358 - 001
+//
+//  Instructor: Husain Gholoom.
+//
+//  This program will create a BST class with the following functions
+//  1. A function that builds a BST by inserting nodes and display the tree.
+//  2. A function that traverses and displays the tree in pre-order form.
+//  3. A function that traverses and displays the tree in post-order form.
+//  4. A function that displays all the right sub roots of that tree.
+//  5. A function that displays all the left sub roots of the tree.
+//  6. A function that counts and displays all the leaves of the tree.
+//  7. A function that displays only the leaves of the tree.
+//
+
