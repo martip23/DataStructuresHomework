5f678885ec5441071ddfb4c57f1f52fd8723db0a
martip23 <patrickmartinez77@gmail.com>
2018-04-17 10:25:23 -0500

    Added function prototypes and implementation stubs

--- Homework6/HomeworkAssignment6/Hillier_Blake_Martinez_Patrick_PG6_BSTs.cpp
+++ Homework6/HomeworkAssignment6/Hillier_Blake_Martinez_Patrick_PG6_BSTs.cpp
@@ -18,3 +18,173 @@
 //  7. A function that displays only the leaves of the tree.
 //
 
+#include <iostream> // For cout
+using namespace std;
+
+/*******************************************************************************
+
+
+
+
+*******************************************************************************/
+class BinarySearchTree() {
+public:
+    int size;
+    int* array;
+
+    // Creates
+    BinarySearchTree(int x);
+
+    // Line comment
+    void insertNode(int x);
+
+    // Line comment
+    void const preOrder();
+
+    // Line comment
+    void const postOrder();
+
+    // Line comment
+    void const displayRSR();
+
+    // Line comment
+    void const displayLSR();
+
+    // Line comment
+    void const treeLeavesCount();
+
+    // Line comment
+    void const displayLeafValues();
+
+    // Deallocates dynamic memory
+    ~BinarySearchTree();
+};
+
+int main () {
+    BinarySearchTree tree(13);
+
+    cout << "Binary Search Tree By Patrick Martinez & Blake Hillier\n\n";
+    cout << "Inserting nodes.\n\n";
+    tree.insertNode(70);
+    tree.insertNode(50);
+    tree.insertNode(100);
+    tree.insertNode(30);
+    tree.insertNode(60);
+    tree.insertNode(80);
+    tree.insertNode(110);
+    tree.insertNode(20);
+    tree.insertNode(68);
+    tree.insertNode(90);
+    tree.insertNode(120);
+    tree.insertNode(25);
+    tree.insertNode(62);
+
+    cout << "Building BST is completed.\n\n";
+
+    cout << "Values of the Binary Search tree.\n\n";
+
+
+
+    // Pre-Order Traversal
+    cout << "Pre-Order Traversal of the BST :\n\n";
+    tree.preOrder();
+
+
+    // Post-Order Traversal
+    cout << "Post-Order Traversal of the BST :\n\n";
+    tree.postOrder();
+
+    // All right sub root values
+    cout << "Here are all the right sub root values for the BST :\n\n";
+    tree.displayRSR();
+
+    // All left sub root values
+    cout << "Here are all the right sub root values for the BST :\n\n";
+    tree.displayLSR();
+
+    // Counting Number of Leaves
+    cout << "\n\nNumber of Leaves =     " << tree.treeLeavesCount();
+
+    // Display the leaf values
+    cout << "\n\nHere are the leaf values in the BST:\n\n";
+    tree.displayLeafValues();
+
+    cout << "\n\nApril 25, 2018\n\n"
+         << "Written by Patrick Martinez & Blake Hillier\n\n";
+
+    return 0;
+}
+
+/** IMPLEMENTATIONS **/
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void BinarySearchTree::insertNode(int x){
+
+}
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void const BinarySearchTree::preOrder(){
+
+}
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void const BinarySearchTree::postOrder(){
+
+}
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void const BinarySearchTree::displayRSR(){
+
+}
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void const BinarySearchTree::displayLSR(){
+
+}
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void const BinarySearchTree::treeLeavesCount(){
+
+}
+
+/*******************************************************************************
+
+BIG COMMENT
+
+*******************************************************************************/
+void const BinarySearchTree::displayLeafValues(){
+
+}
+
+/*******************************************************************************
+Deallocates dynamic memory
+Inputs: None
+Outputs: None
+*******************************************************************************/
+BinarySearchTree::~BinarySearchTree(){
+    delete[] array;
+}
